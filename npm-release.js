49979b4da95c0876cd92ad34ef2e95fca13db0de
Noah Isaacson <noah@nisaacson.com>
2013-04-23 12:26:41 -0400

    added absolute repo path to npm-release commands

--- npm-release.js
+++ npm-release.js
@@ -1,77 +1,79 @@
+var path = require('path')
 var fs = require('fs');
-var child_process = require('child_process'); 
+var child_process = require('child_process');
 
 var PACKAGE_JSON_FILE = 'package.json';
 var JSON_INDENT = 4;
-
+var repoPath = path.join(__dirname, '../..')
 function buildTagCmd(tagName) {
-    return 'git tag ' + tagName
+  return '(cd ' + repoPath + ' && git tag ' + tagName
 }
 
 function buildCommitCmd(message) {
-    return 'git add package.json; git commit -m "' + message + '" package.json; git push origin master'
+
+  return '(cd '+ repoPath + ' && git add package.json && git commit -m "' + message + '" package.json && git push origin master)'
 }
 
 function commitPackageFile(callback) {
-    var cmd = buildCommitCmd('Incrementing version number for next iteration')
-    exec(cmd, callback)
+  var cmd = buildCommitCmd('Incrementing version number for next iteration')
+  exec(cmd, callback)
 }
 
 function tagRepo(tagName, callback) {
-    var tagCmd = buildTagCmd(tagName)
-    exec(tagCmd, callback)
+  var tagCmd = buildTagCmd(tagName)
+  exec(tagCmd, callback)
 }
 
 function exec(cmd, callback) {
-    console.log("Running %s", cmd)
-    child_process.exec(cmd, callback)    
+  console.log("Running %s", cmd)
+  child_process.exec(cmd, callback)
 }
 
 function readJSON(file) {
-    var jsonStr = fs.readFileSync(PACKAGE_JSON_FILE)
-    return JSON.parse(jsonStr);
+  var jsonStr = fs.readFileSync(PACKAGE_JSON_FILE)
+  return JSON.parse(jsonStr);
 }
 
 function writeJSON(file) {
-    var jsonStr = JSON.stringify(file, null, JSON_INDENT)
-    fs.writeFileSync(PACKAGE_JSON_FILE, jsonStr)
+  var jsonStr = JSON.stringify(file, null, JSON_INDENT)
+  fs.writeFileSync(PACKAGE_JSON_FILE, jsonStr)
 }
 
 function incrementVersionInJson(packageJson) {
-    var version = packageJson.version.split('.');
-    version[version.length-1]++;
-    var newVersion = version.join('.');
-    packageJson.version = newVersion;
-    return newVersion;    
+  var version = packageJson.version.split('.');
+  version[version.length-1]++;
+  var newVersion = version.join('.');
+  packageJson.version = newVersion;
+  return newVersion;
 }
 
 function tagAndIncrementVersion(callback) {
-    var packageJson = readJSON(PACKAGE_JSON_FILE);
-    var tagName = packageJson.name + '-' + packageJson.version
-    tagRepo(tagName, function (error, stdout, stderr) {
-        var newVersion = incrementVersionInJson(packageJson)
-        console.log("Updating version in package.json to %s", newVersion)
-        writeJSON(packageJson)
-        commitPackageFile(function (error, stdout, stderr) {
-            callback(packageJson.name, tagName)
-        })    
+  var packageJson = readJSON(PACKAGE_JSON_FILE);
+  var tagName = packageJson.name + '-' + packageJson.version
+  tagRepo(tagName, function (error, stdout, stderr) {
+    var newVersion = incrementVersionInJson(packageJson)
+    console.log("Updating version in package.json to %s", newVersion)
+    writeJSON(packageJson)
+    commitPackageFile(function (error, stdout, stderr) {
+      callback(packageJson.name, tagName)
     })
+  })
 }
 
 function checkoutAndPack(packageName, newTag) {
-    var cmd = "rm -rf target; mkdir target; cd target; git clone ../.git code >/dev/null; cd code; git checkout " + newTag + " >/dev/null; npm publish;"
-    exec(cmd, function (error, stdout, stderr) {
-        var fileName = stdout.replace('./','').replace("\n", '')
-         pushTag(newTag)
-    })
+  var cmd = '(cd ' + repoPath + ' && rm -rf target && mkdir target && cd target && git clone ../.git code >/dev/null && cd code && git checkout ' + newTag + ' >/dev/null && npm publish)'
+  exec(cmd, function (error, stdout, stderr) {
+    var fileName = stdout.replace('./','').replace('\n', '')
+    pushTag(newTag)
+  })
 }
 
 function pushTag(tagName) {
-    var cmd = 'git push origin ' + tagName
-    child_process.exec(cmd, function (error, stdout, stderr) {
-    })
+  var cmd = '(cd ' + repoPath + ' && git push origin ' + tagName
+  child_process.exec(cmd, function (error, stdout, stderr) {
+  })
 }
 
 tagAndIncrementVersion(function(packageName, newTag) {
-    checkoutAndPack(packageName, newTag);   
+  checkoutAndPack(packageName, newTag);
 })
